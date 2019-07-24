f117b8894f4f2691e098dec1a42320d8c5c6a112
matoruru <matoruru@shgt.jp>
2019-07-24 19:00:30 +0900

    To prevent stack over flow

--- src/Util.purs
+++ src/Util.purs
@@ -6,28 +6,26 @@ module Util
 import Prelude
 
 import Data.Array (elem, sort, (..))
-import Data.Traversable (traverse_)
 import Data.Tuple (Tuple(..), snd)
-import Effect (Effect)
+import Effect (Effect, foreachE)
 import Effect.Uncurried (EffectFn1, runEffectFn1)
 import Type (Width, Pos)
 
 foreign import consoleClear :: Effect Unit
 
 foreign import logNoNewlineImpl :: EffectFn1 String Unit
+
 logNoNewline :: String -> Effect Unit
 logNoNewline = runEffectFn1 logNoNewlineImpl
 
 showPattern :: Width -> Array Pos -> Effect Unit
 showPattern n ps = do
-  traverse_ ( \p -> case elem p ps of
-                true  -> logNoNewline $ "*" <> newLine p
-                false -> logNoNewline $ "_" <> newLine p
-            ) $ wholeBoard n
-    where
-      newLine p
-        | snd p == (n - 1) = "\n"
-        | otherwise        = ""
+  foreachE ( map ( \p -> case elem p ps of
+                           true  -> "■"
+                           false -> " "
+                         <> if snd p == n - 1 then "\n" else ""
+                 ) <<< wholeBoard $ n
+           ) logNoNewline
 
 wholeBoard :: Width -> Array Pos
 wholeBoard n = do

b13fcb104b6fa3ce856e4fa7baadbce04d60c972
matoruru <matoruru@shgt.jp>
2019-08-01 20:54:06 +0900

    Added pulsar

--- src/GameOfLife/Pattern/Oscillator.purs
+++ src/GameOfLife/Pattern/Oscillator.purs
@@ -12,6 +12,11 @@ trafficLight :: Pattern
 trafficLight = [ Tuple 0 3, Tuple 0 4, Tuple 0 5, Tuple 4 0, Tuple 4 1, Tuple 4 2, Tuple 8 3, Tuple 8 4, Tuple 8 5, Tuple 4 6, Tuple 4 7, Tuple 4 8 ]
 
 
+-- period 3
+pulsar :: Pattern
+pulsar = [ Tuple 5 7, Tuple 6 7, Tuple 7 7, Tuple 8 7, Tuple 9 7, Tuple 5 8, Tuple 9 8 ]
+
+
 -- period 8
 nebula :: Pattern
 nebula = [ Tuple 2 2, Tuple 3 2, Tuple 4 2, Tuple 5 2, Tuple 6 2, Tuple 7 2, Tuple 2 3, Tuple 3 3, Tuple 4 3, Tuple 5 3, Tuple 6 3, Tuple 7 3, Tuple 2 5, Tuple 2 6, Tuple 2 7, Tuple 2 8, Tuple 2 9, Tuple 2 10, Tuple 3 5, Tuple 3 6, Tuple 3 7, Tuple 3 8, Tuple 3 9, Tuple 3 10, Tuple 5 9, Tuple 6 9, Tuple 7 9, Tuple 8 9, Tuple 9 9, Tuple 10 9, Tuple 5 10, Tuple 6 10, Tuple 7 10, Tuple 8 10, Tuple 9 10, Tuple 10 10, Tuple 9 2, Tuple 9 3, Tuple 9 4, Tuple 9 5, Tuple 9 6, Tuple 9 7, Tuple 10 2, Tuple 10 3, Tuple 10 4, Tuple 10 5, Tuple 10 6, Tuple 10 7 ]

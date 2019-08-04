a51159a1ca63c861bffc956590e67fcf0cc4d771
matoruru <matoruru@shgt.jp>
2019-08-04 16:06:01 +0900

    Modified a pattern

--- src/GameOfLife/Pattern/Oscillator.purs
+++ src/GameOfLife/Pattern/Oscillator.purs
@@ -14,7 +14,7 @@ trafficLight = [ Tuple 0 3, Tuple 0 4, Tuple 0 5, Tuple 4 0, Tuple 4 1, Tuple 4 
 
 -- period 3
 pulsar :: Pattern
-pulsar = [ Tuple 5 7, Tuple 6 7, Tuple 7 7, Tuple 8 7, Tuple 9 7, Tuple 5 8, Tuple 9 8 ]
+pulsar = [(Tuple 5 6),(Tuple 6 5),(Tuple 5 8),(Tuple 6 9),(Tuple 8 5),(Tuple 9 6),(Tuple 8 9),(Tuple 9 8),(Tuple 1 4),(Tuple 4 1),(Tuple 1 10),(Tuple 4 13),(Tuple 10 1),(Tuple 13 4),(Tuple 13 10),(Tuple 10 13),(Tuple 4 6),(Tuple 6 4),(Tuple 4 8),(Tuple 6 10),(Tuple 8 4),(Tuple 10 6),(Tuple 8 10),(Tuple 10 8),(Tuple 0 4),(Tuple 2 4),(Tuple 0 10),(Tuple 2 10),(Tuple 12 4),(Tuple 14 4),(Tuple 12 10),(Tuple 14 10),(Tuple 4 0),(Tuple 4 2),(Tuple 4 12),(Tuple 4 14),(Tuple 10 0),(Tuple 10 2),(Tuple 10 12),(Tuple 10 14),(Tuple 2 5),(Tuple 4 5),(Tuple 5 2),(Tuple 5 4),(Tuple 2 9),(Tuple 4 9),(Tuple 5 10),(Tuple 5 12),(Tuple 9 2),(Tuple 9 4),(Tuple 10 5),(Tuple 12 5),(Tuple 9 10),(Tuple 9 12),(Tuple 10 9),(Tuple 12 9)]
 
 
 -- period 8
